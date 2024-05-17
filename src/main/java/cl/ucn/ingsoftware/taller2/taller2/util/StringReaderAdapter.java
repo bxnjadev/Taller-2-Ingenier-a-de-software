bd65171b8934fa28599f5f702f6c0baa441e3869
Benjamín Miranda <supremobenjamin.cia@gmail.com>
2024-05-17 13:15:28 -0400

    feat(core): finished read system

---
+++ src/main/java/cl/ucn/ingsoftware/taller2/taller2/util/StringReaderAdapter.java
@@ -0,0 +1,30 @@
+package cl.ucn.ingsoftware.taller2.taller2.util;
+
+import cl.ucn.ingsoftware.taller2.taller2.HelloApplication;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class StringReaderAdapter implements Reader {
+
+    private final  Reader reader;
+
+    public StringReaderAdapter(String fileName) {
+        InputStream inputStream = HelloApplication
+                .class.getResourceAsStream(
+                        fileName
+        );
+
+        reader = new DefaultReader(inputStream);
+    }
+
+    @Override
+    public String readLine() throws IOException {
+        return reader.readLine();
+    }
+
+    @Override
+    public void close() throws IOException {
+        reader.close();
+    }
+}
